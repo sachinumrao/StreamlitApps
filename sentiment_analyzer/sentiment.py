a66304d6be45eb88c9ba1ae3f455311b3663a193
Sachin Umrao <sachin.umrao.1512@gmail.com>
2019-10-19 23:58:25 +0530

    Added Streamlit app for vader based sentiment analyzer

---
+++ sentiment_analyzer/sentiment.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Create sentiment Analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    score = {}
+    score = analyzer.polarity_scores(text)
+    score.pop('compound')
+    sentiment_map = {'pos':'Positive',
+                    'neg': 'Negative',
+                    'neu': 'Neutral'}
+    sent = max(score, key=lambda key: score[key])
+    return sentiment_map[sent]
+
+
+# Create title and header for page
+st.title('Sentment Analyzer')
+st.header('What Do You Have in Mind...')
+
+# Get text from user
+txt = st.text_input('','')
+
+sentiment = None
+
+# Get Sentiment button
+if st.button('Get Sentiment'):
+    sentiment = get_sentiment(txt)
+
+# Display sentiment in webpage
+st.write('Sentiment:', sentiment)
+
+# Ballons if sentiment is positive
+if sentiment is 'Positive':
+    st.balloons()
+
